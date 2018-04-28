fe00637aa29d2c2e9fc2c2daa359e344419e0b21
TomM <tom.mingasson@epfl.ch>
2018-04-28 19:32:08 +0200

    wesh4

--- trainNetwork.m
+++ trainNetwork.m
@@ -26,18 +26,38 @@ switch Params.stoppingCriteria
         % stopping criteria = ||updateStep(t+1) - updateStep(t)|| > tolUpdateStep
         
         t = 1;
-        updateSteps(1,1) = inf;
-        updateStep = Params.tolUpdateStep + 1;
-        while updateStep > Params.tolUpdateStep && t<Params.maxIter
+        updateStepMean = zeros(1, Params.maxIter);
+        updateStepMeanDelta = zeros(1, Params.maxIter);
+        continueTraining = true;
+        while continueTraining
+            
             i=iR(t);
+            
             new_centers = som_step(old_centers, Data.data(i,:), Params.neighbor, Params.eta, Params.sigma);
-            updateSteps(1,t+1) = getUpdateStep(new_centers, old_centers);
+            updateSteps(1,t) = getUpdateStep(new_centers, old_centers);
             old_centers = new_centers;
-            updateStep = abs(updateSteps(t+1)-updateSteps(t));
+            
+            if t==1
+                 updateStepMean_t = updateSteps(1,t);
+                 updateStepMeanDelta_t =  updateSteps(1,t);
+            end
+            
+            if mod(t,Params.tolUpdateMeanWindow) == 0
+                updateStepMean_t = mean(updateSteps(1,t-Params.tolUpdateMeanWindow+1:t));
+                updateStepMeanDelta_t = abs(updateStepMean(1,t) - updateStepMean(1,t-1));
+            end
+            
+            updateStepMean(1,t) = updateStepMean_t;
+            updateStepMeanDelta(1,t) = updateStepMeanDelta_t;
+            
             t = t+1;
+            
             if Params.displayTraining && (mod(t, Params.displayStep)==0 || t==1)
                 visualizeTrainedNetwork(old_centers, ['network state at iteration: ', num2str(t)], 100)
+                visualizeUpdateSteps(updateSteps, updateStepMean, updateStepMeanDelta, t, 101)
             end
+            
+            continueTraining = updateStepMean_t > Params.tolUpdateStep && t<Params.maxIter;
             
         end
         updateSteps = updateSteps(2:t);
@@ -47,13 +67,6 @@ end
 % store
 Results.centers = new_centers;
 Results.updateSteps = updateSteps;
-
-% visualize
-figure
-plot(updateSteps)
-xlabel('iterations')
-ylabel('cost function')
-grid on
 
 end
 
---
+++ visualizeUpdateSteps.m
@@ -0,0 +1,24 @@
+function visualizeUpdateSteps(updateSteps, updateStepMean, updateStepMeanDelta, iteration, figureIndex)
+
+figure(figureIndex)
+
+subplot(311)
+plot(updateSteps(1:iteration))
+xlabel('iterations')
+title('||centers_{old} - centers_{new}||')
+grid on
+
+subplot(312)
+plot(updateStepMean(1:iteration))
+xlabel('iterations')
+title('mean(||centers_{old} - centers_{new}||) over')
+grid on
+
+subplot(313)
+plot(updateStepMeanDelta(1:iteration))
+xlabel('iterations')
+title('mean difference over iteration')
+grid on
+
+end
+
